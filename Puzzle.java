bbf63f1a3905d771301c55338c5a183a47bb3ea1
RuiwenT <ruiwentang@gmail.com>
2022-01-19 00:42:18 -0500

    + Puzzle.java to root

---
+++ Puzzle.java
@@ -0,0 +1,32 @@
+public class Puzzle{
+
+    public int wordCount;
+    public String[] words;
+    public String[] clues;
+    public String[][] outputArr;
+
+    public Puzzle(){
+
+    }
+
+    public Puzzle(String[] _words, String[] _clues){
+
+    }
+
+    public Puzzle(String[][] _outputArr){
+
+    }
+
+    public String toString(){
+        return "";
+    }
+
+    public void fillWords(String[] _words){
+
+    }
+
+    public void fillRand(){
+
+    }
+
+}
