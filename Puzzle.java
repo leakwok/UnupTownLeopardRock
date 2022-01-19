341242c697f2b58d154cfa27b0ae9df68ff96a4b
Ruiwen-T <you@example.com>
2022-01-19 13:19:44 -0500

    + devlog

--- Puzzle.java
+++ Puzzle.java
@@ -1,16 +1,23 @@
+import java.util.ArrayList; 
+
 public class Puzzle{
 
-    public int wordCount;
-    public String[] words;
-    public String[] clues;
-    public String[][] outputArr;
+    public int _wordCount;
+    public ArrayList<String> _words;
+    public ArrayList<String> _clues;
+    public String[][] _outputArr;
 
     public Puzzle(){
-
     }
 
-    public Puzzle(String[] _words, String[] _clues){
-
+    public Puzzle(ArrayList<String> words, ArrayList<String> clues){
+	for(String s : words){
+		_words.add(s);
+	}
+	for(String s : clues){
+		_clues.add(s);
+	}
+	_wordCount = _words.size();
     }
 
     public Puzzle(String[][] _outputArr){
@@ -18,7 +25,14 @@ public class Puzzle{
     }
 
     public String toString(){
-        return "";
+        String s = "";
+	for(String[] a : _outputArr){
+		for(String g : a){
+			s += g + " ";
+		}
+		s += "\n";
+	}
+	return s;
     }
 
     public void fillWords(String[] _words){
